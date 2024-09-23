98a1ee1c410c4c3c695163e1eda67fb4c5d95a98
raghavendru <raghavendruppar@gmail.com>
2024-09-23 14:04:24 +0530

    built user card feed

---
+++ src/components/UserCard.jsx
@@ -0,0 +1,26 @@
+
+const UserCard = ({user}) => {
+     const {firstName,lastName,age,gender,photoUrl,about,skills} = user;
+    console.log(user);
+  return (
+    <div className="card bg-base-300 w-96 shadow-xl">
+    <figure>
+      <img
+        src={user.photoUrl}
+        alt="photo" />
+    </figure>
+    <div className="card-body">
+      <h2 className="card-title">{firstName + " " + lastName }</h2>
+      {age && gender &&<p>{age + ", " + gender}</p>}
+      <p>{about}</p>
+      <div className="card-actions justify-center my-4">
+      <button className="btn btn-primary">Ignore</button>
+
+        <button className="btn btn-secondary">Interested</button>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default UserCard
