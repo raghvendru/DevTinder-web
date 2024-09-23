c39deca3129efccceabef50e846a31852ea53f55
raghavendru <raghavendruppar@gmail.com>
2024-09-23 12:51:19 +0530

    redirect if not authenticated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import { BrowserRouter, Routes,Route } from "react-router-dom"
-import Body from "./Body"
-import Login from "./Login"
-import Profile from "./Profile"
+import Body from "./components/Body"
+import Login from "./components/Login"
+import Profile from "./components/Profile"
+import { Provider } from "react-redux"
+import appStore from "./utils/appStore"
+import Feed from "./components/Feed"
 
 
 
@@ -9,9 +12,12 @@ function App() {
 
   return (
     <>
+    <Provider store={appStore}>
     <BrowserRouter basename="/">
     <Routes>
     <Route path="/" element={<Body/>}>
+    <Route path="/" element={<Feed/>}/>
+
 
        <Route path="/login" element={<Login/>}/>
        <Route path="/profile" element={<Profile/>}/>
@@ -25,6 +31,7 @@ function App() {
 
     </Routes>
     </BrowserRouter>
+    </Provider>
      
      
       
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,35 +1,43 @@
+import { useSelector } from "react-redux"
+import { Link } from "react-router-dom"
 
 const NavBar = () => {
+  const user = useSelector(store=>store.user)
   return (
     <div className="navbar bg-accent">
     <div className="flex-1">
-      <a className="btn btn-ghost text-xl">🧑‍💻 DevTinder</a>
+      <Link to="/" className="btn btn-ghost text-xl">🧑‍💻 DevTinder</Link>
     </div>
+    {user &&(
     <div className="flex-none gap-2">
-      <div className="form-control">
+      <div className="form-control">Welcome,{user.firstName}
       </div>
-      <div className="dropdown dropdown-end mx-5">
+      
+      <div className="dropdown dropdown-end mx-5 flex ">
+
         <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
           <div className="w-10 rounded-full">
             <img
-              alt="Tailwind CSS Navbar component"
-              src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+              alt="user photo"
+              src={user.photoUrl} />
           </div>
         </div>
         <ul
           tabIndex={0}
           className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
           <li>
-            <a className="justify-between">
+            <Link to="/profile" className="justify-between">
               Profile
               <span className="badge">New</span>
-            </a>
+            </Link>
           </li>
           <li><a>Settings</a></li>
           <li><a>Logout</a></li>
         </ul>
       </div>
+      
     </div>
+    )}
   </div>
   )
 }
